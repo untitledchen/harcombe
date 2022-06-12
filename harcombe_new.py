0dcb151b5812341d931c79ee46b8cb5a4952e2e3
untitledchen <untitledchen@gmail.com>
2022-06-11 23:24:46 -0500

    harcombe_new.py

---
+++ harcombe_new.py
@@ -0,0 +1,18 @@
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def odes(x, t, r, tau_lag):
+    
+    M = x[0]
+    E = x[1]
+    A = x[2]
+    L = x[3]
+    S = x[4]
+
+    dMdt = 
+    dEdt =
+    dAdt =
+    dLdt =
+    dSdt =
