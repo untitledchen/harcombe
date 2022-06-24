464df6f8fbf80031efc74a0a9ab43278c78ff9e2
untitledchen <untitledchen@gmail.com>
2022-06-24 15:37:51 -0500

    new functions

--- hcb_emono.py
+++ hcb_emono.py
@@ -6,13 +6,14 @@ import random
 import math
 
 ### define odes equation
-def odes(x, t, alpha, nE, tau_lag):
+def odes(x, t, alpha, nE, tau_lag, frid=False):
     '''
     x: list of initial conditions
     t: list of times
     alpha: additional death rate due to antibiotic, as a proportion of max growth rate r
     nE: number of strains of E. coli
     tau_lag: list of lag times of each strain # all identical for now
+    frid: when True, dMdt and dLdt = 0 (for use with Fridman-style analysis)
     '''
     
     M = x[0]
@@ -51,14 +52,14 @@ def odes(x, t, alpha, nE, tau_lag):
     for i in range(1, nE + 1):
         sigma_cM += locals()[f'cM{i}']*locals()[f'Eg{i}']*(M/(M+K_M))*(L/(L+K_L))
         
-    dMdt = -sigma_cM - kM*M
+    dMdt = (-sigma_cM - kM*M) * ([1, 0][frid])
     
     # L
     sigma_cL = 0
     for i in range(1, nE + 1):
         sigma_cL += locals()[f'cL{i}']*locals()[f'Eg{i}']*(M/(M+K_M))*(L/(L+K_L))
         
-    dLdt = -sigma_cL - kL*L
+    dLdt = (-sigma_cL - kL*L) * ([1, 0][frid])
 
     to_return = [dMdt,dLdt]
     for i in range(1, nE + 1): ## could probably use list comprehension instead of this for loop
@@ -69,22 +70,18 @@ def odes(x, t, alpha, nE, tau_lag):
 ###
 
 ### phase 1: antibiotic
-def run_phase1(odes, init_cond, t_interval, nE, tau_lag):
-    print(f'Running phase 1: antibiotic, with {init_cond}')
+def run_phase1(odes, init_cond, t_interval, nE, tau_lag, frid=False):
     
-    sol = odeint(odes, init_cond, t_interval, args=(2, nE, tau_lag)) ## when alpha = -2, effective growth rate = -r
+    sol = odeint(odes, init_cond, t_interval, args=(2, nE, tau_lag, frid)) ## when alpha = -2, effective growth rate = -r
     para = [t_interval, nE, tau_lag]
     return sol, para
 
 ### phase 2: no antibiotic
-def run_phase2(odes, sol1, t_interval, nE, tau_lag):
+def run_phase2(odes, sol1, t_interval, nE, tau_lag, frid=False):
     
-    init_cond = [sol1[:, 0][-1], sol1[:, 1][-1]]
-    for i in range(1, nE + 1): ## could maybe use list comp
-        init_cond += [sol1[:, (2*i)][-1], sol1[:, (1 + 2*i)][-1]]
-    print(f'Running phase 2: no antibiotic, with {init_cond}')
+    init_cond = sol1[-1, :] ## array not list
     
-    sol = odeint(odes, init_cond, t_interval, args=(0, nE, tau_lag))
+    sol = odeint(odes, init_cond, t_interval, args=(0, nE, tau_lag, frid))
     para = [t_interval, nE, tau_lag]
     return sol, para
 
@@ -92,7 +89,7 @@ def run_phase2(odes, sol1, t_interval, nE, tau_lag):
 def solplot(sols, paras):
     '''
     sols:list of solutions for each phase
-    t_intervals:list of t_intervals respective to sols
+    paras:list of parameters for each phase [t_interval, nE, tau_lag]
     '''
     fig, axs = plt.subplots(nrows=1, ncols=len(sols), figsize=(10, 4))
 
@@ -140,17 +137,18 @@ def solplot(sols, paras):
     plt.show()
 
 ### run regular: observe as is
-def regular(init_M, init_L, Ta, nE=1, tau_lag_inp=None):
+def regular(init_M, init_L, Ta, nE=1, tau_lag=None, frid=False):
     '''
     init_M: initial methionine concentration
     init_L: initial lactose concentration
     Ta: length of phase 1
-    tau_lag: lag time, shared by all strains; or (default) lag time for each strain is a random value between 0 and 12 inclusive
+    tau_lag:list of lag times for each strain; or (default) lag time for each strain is a random value between 0 and 12 inclusive
+    frid: when True, dMdt and dLdt = 0 (for use with optimized())
     '''
-    if tau_lag_inp == None:
+    if tau_lag == None:
         tau_lag = [random.uniform(0, 12) for i in range(nE)]
     else:
-        tau_lag = [tau_lag_inp for i in range(nE)]
+        nE = len(tau_lag)
         
     init_cond = [init_M, init_L] + [0, 1]*nE ## each strain starts with growing population 0 and lagging population 1
 
@@ -159,13 +157,102 @@ def regular(init_M, init_L, Ta, nE=1, tau_lag_inp=None):
     t_interval2 = np.linspace(0, 15, 1000) #
 
     # run phases
+    print(f'Running phase 1: antibiotic, with {init_cond}')
     sol1, para1 = run_phase1(odes, init_cond, t_interval1, nE, tau_lag)
-    
-    sol2, para2 = run_phase2(odes, sol1, t_interval2, nE, tau_lag)
+
+    print(f'Running phase 2: no antibiotic, with {sol1[-1, :]}')
+    sol2, para2 = run_phase2(odes, sol1, t_interval2, nE, tau_lag, frid=frid)
 
     #plot
     solplot([sol1, sol2], [para1, para2])
     
-### run optimized: adapted to Fridman analysis
-###
-regular(10, 10, 6, nE=3)
+### run taus: Fridman analysis pt 1
+def taus(init_M, init_L, Ta, nE=1, taulim=12, plot=True):
+    '''
+    init_M: initial methionine concentration
+    init_L: initial lactose concentration
+    Ta: length of phase 1
+    taulim: limit of tau_lags to try
+    3d: switch to recreating graph 3d in Fridman main paper
+
+    returns:list of max n*0, opt tau_lag
+    '''
+    tlim = 15 #
+
+    taus = np.linspace(0, taulim, 100)[1:] ## lowered accuracy with 100 instead of 1000 for speed
+    init_cond = [init_M, init_L] + [0, 1]*nE ## each strain starts with growing population 0 and lagging population 1
+
+    # time intervals
+    t_interval1 = np.linspace(0, Ta, 1000)
+    t_interval2 = np.linspace(0, tlim, 1000)
+
+    # run phases and append n*0 to list(n_0)
+    for i in range(1, nE + 1):
+        locals()[f'n_0{i}'] = []
+    
+    for tau in taus:
+        tau_lag = [tau for i in range(1, nE + 1)]
+        
+        sol1, para1 = run_phase1(odes, init_cond, t_interval1, nE, tau_lag)
+        sol2, para2 = run_phase2(odes, sol1, t_interval2, nE, tau_lag, frid=True)
+        for i in range(1, nE + 1):
+            locals()[f'total{i}'] = sol2[:, (2*i)] + sol2[:, (1 + 2*i)]
+        for i in range(1, nE + 1):
+            locals()[f'n_0{i}'].append((math.e)**-tlim * (locals()[f'total{i}'][-1])) ## calculate n*0 using the last t value available (== tlim2)
+
+    if plot:     
+        # plot
+        g = 0.8/(2*nE)
+        for i in range(1, nE + 1):
+            plt.plot(taus, locals()[f'n_0{i}'], color = (0, 0.8 - g*i, 0), label=f'E. coli strain {i}, lag time = equal')
+
+        plt.ylabel('n*0')
+        plt.xlabel('tau_lag')
+        plt.legend()
+
+        plt.show()
+        
+    return [max(locals()[f'n_01']), taus[locals()[f'n_01'].index(max(locals()[f'n_01']))]]
+
+### run 3d: Fridman analysis pt 2
+def threed(init_M, init_L, nE=1, taulim=12, Talim=12):
+    Tas = [Ta for Ta in range(1, Talim + 1)] # list of Tas to try, whole numbers only
+
+    opt_taus = []
+    for Ta in Tas:
+        opt_taus.append(taus(init_M, init_L, Ta, nE=1, taulim=12, plot=False)[1])
+
+    # plot
+    plt.plot(Tas, opt_taus, label=f'init_M: {init_M} init_L: {init_L}')
+
+    plt.ylabel('Optimal tau_lag')
+    plt.xlabel('Ta')
+    
+### user interface
+inp = input("Run Sydney's test track (s) or run custom (c)?: ").strip()
+if inp == 's':
+    '''
+    print('Running regular(10, 10, 6, tau_lag=[3,6,9]). Initial methionine: 1; initial lactose: 1; Ta: 6; strains: 3; tau_lag: 3, 6, 9.')
+    regular(10, 10, 6, tau_lag=[3,6,9])
+    print('Running regular(10, 10, 6, nE=3). Initial methionine: 1; initial lactose: 1; Ta: 6; strains: 3; tau_lag: random.')
+    regular(10, 10, 6, nE=3)
+    print('Running regular(10, 10, 6, nE=3, frid=True). Same parameters but phase 2 is adapted for Fridman analysis.')
+    regular(10, 10, 6, nE=3, frid=True)
+    print('Running taus(10, 10, 6, nE=3). Returns max n*0 and optimal tau_lag, respectively.')
+    print(taus(10, 10, 6, nE=3))
+    print('Running taus(10, 10, 6, nE=5).')
+    print(taus(10, 10, 6, nE=5))
+    print('Running taus(100, 100, 6, nE=3).')
+    print(taus(100, 100, 6, nE=3))
+    '''
+    print('Running threed(10, 10, nE=3) and threed(100, 100, nE=3)')
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    threed(10, 10, nE=3)
+    threed(100, 100, nE=3)
+    ax.set_aspect('equal', adjustable='box')
+    plt.legend()
+    plt.show()
+    
+elif inp == 'c':
+    pass
