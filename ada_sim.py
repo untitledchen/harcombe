7c2966dc77479bf687ae3d6fb62af8573654941f
untitledchen <untitledchen@gmail.com>
2022-07-21 14:55:13 -0500

    'calculate_fitness'

--- ada_sim.py
+++ ada_sim.py
@@ -30,8 +30,36 @@ def Species(N=1000, u=0.001, first_genotype=None):
             print('All genotypes must contain the following fields: name (character), n (integer), s (number), MIC (number), ancestors (character)')
             return 0
 
-    me = SpeciesType(me)##
+    me = SpeciesType(me)
     return me
+
+def get_s(Species):
+    if type(Species) == SpeciesType:
+        return pd.Series([x['s'] for x in Species['genotypes']])
+    else:
+        print('This method requires a SpeciesType object')
+        return Species
+
+def get_n(Species):
+    if type(Species) == SpeciesType:
+        return pd.Series([x['n'] for x in Species['genotypes']])
+    else:
+        print('This method requires a SpeciesType object')
+        return Species
+
+def get_MIC(Species):
+    if type(Species) == SpeciesType:
+        return pd.Series([x['MIC'] for x in Species['genotypes']])
+    else:
+        print('This method requires a SpeciesType object')
+        return Species
+
+def get_ancestors(Species):
+    if type(Species) == SpeciesType:
+        return pd.Series([x['ancestors'] for x in Species['genotypes']])
+    else:
+        print('This method requires a SpeciesType object')
+        return Species
 
 def make_first_season(wells, n_species, current_prefixes, u=0.0025):
     next_season = pd.Series(dtype=object)
@@ -47,26 +75,46 @@ def make_first_season(wells, n_species, current_prefixes, u=0.0025):
 
     return next_season
 
+def calculate_fitness(s, n, MIC, antibiotic):
+    s[MIC < antibiotic] = 0
+    
+    if all(s==0):
+        print('All individuals have zero fitness')
+        return -1
+    w = s/sum(s)*n
+    w = w/sum(w)
+    return w.cumsum()
+
 def run_one_simulation(species, gens, antibiotic, mutant_func, interdependent=True, current_prefix=''):
     n_species = len(species)
     pop_sizes = [x['N'] for x in species]
 
-    if len(antibiotic) == 1:
-        antibiotic = [antibiotic]*gens
+    ## removed the len check
+    antibiotic = [antibiotic]*gens
 
     if interdependent: ##unchecked
         if any([y == 0 for y in [len(x['genotypes']) for x in species]]):
             for k in range(1, n_species+1):
                 results[f'{k}'] = pd.DataFrame({'gen':-1, 'genotypes':'-1', 'freq':-1, 's':-1, 'MIC':-1, 'ancestors':'-1'}, index=[0])
             return results
+
+    results = pd.Series(dtype=object)
+    for k in range(1, n_species+1):
+        results[f'{k}'] = pd.DataFrame({'gen':0, 'genotypes': species[f'{k}'], 'freq': [x/species[f'{k}']['N'] for x in get_n(species[f'{k}'])], 's': get_s(species[f'{k}']), 'MIC': get_MIC(species[f'{k}']), 'ancestors': get_ancestors(species[f'{k}'])}, index=[0])
+
+    for gen in range(1, gens+1):
+        w = pd.Series(map(lambda x: calculate_fitness(get_s(x), get_n(x), get_MIC(x), antibiotic[gen-1]), species))
         
+        if interdependent:
+            pass
+            
 ### simulation parameters
-reps = 5 # reps per treatment condition
+reps = 1 # reps per treatment condition ##
 N = 1000 # individuals per species
 u = 0.001 # mutation rate 
-max_interdependent_species = 3 #
-seasons = 20 # number of transfers
-gens = 20 # gens per well per season
+max_interdependent_species = 2 # ##
+seasons = 2 # number of transfers ##
+gens = 3 # gens per well per season ##
 wells = 15 #
 antibiotic_change_per_well = 1 # antibiotic concentration increase per well
 
@@ -93,15 +141,17 @@ current_prefixes = [f'w{well}g1' for well in range(1, wells+1)]
 all_data = pd.DataFrame()
 for rep in range(1, reps+1):
     for n_species in numbers_of_species:
+        '''
         print('rep', rep)
         print('\tgens', gens)
         print('\tu', u)
         print('\tmutant func', mutant_func_name)
         print('\tn_species', n_species)
+        '''
 
         next_season = make_first_season(wells, n_species, current_prefixes, u = u)
 '''
         final = pd.DataFrame()
         for season in range(1, seasons+1):
-            results = pd.Series(map(lambda x: run_one_simulation(next_season[f'{x}'], gens, antibiotic[x-1], mutant_func, TRUE, current_prefixes[x-1]), [i for i in range(1, wells + 1)]))
-      '''       
+            results = pd.Series(map(lambda x: run_one_simulation(next_season[f'{x}'], gens, antibiotic[x-1], mutant_func, True, current_prefixes[x-1]), [i for i in range(1, wells + 1)]))
+   '''
